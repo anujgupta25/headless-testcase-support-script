d299b1cf3f8e5083ddb7217075414601aca0ee77
abhayhayarancedcoss <abhayhayaran@cedcoss.com>
2023-04-03 23:50:09 +0530

    first e2e test

---
+++ e2e-tests/src/main/java/Demo/Cedcommerce/Resources/BaseClass.java
@@ -0,0 +1,39 @@
+package Demo.Cedcommerce.Resources;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+public class BaseClass {
+
+	protected Utilities util = Utilities.getObject();
+
+	protected Properties prop;
+
+	@BeforeClass(alwaysRun = true)
+	public void LaunchingMethod() throws Exception {
+		util.openFirstPage(util, "chrome", null, util.getProperty("BaseUrl"));
+	}
+	
+	@AfterClass(alwaysRun = true)
+	public void QuitMethod() {
+		util.closeBrowser();
+	}
+
+	public String getScreenShotPath(String testCaseName, WebDriver driver) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot) util.getDriver();
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		String destinationFile = System.getProperty("user.dir") + "reports/" + testCaseName + ".png";
+		FileUtils.copyFile(source, new File(destinationFile));
+		return destinationFile;
+	}
+}
