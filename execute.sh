41a6835e4af8595ab493d524e250abd9d5ea5072
Anuj Gupta <anujgupta@cedcoss.com>
2023-06-05 05:25:21 +0000

    added not null condition

--- execute.sh
+++ execute.sh
@@ -2,7 +2,13 @@
 AWS_PULL_REQUEST_ID=$1
 AWS_CLONE_URL=$2
 
-echo "AWS PULL REQUEST ID: ${AWS_PULL_REQUEST_ID}, AWS_CLONE_URL: ${AWS_CLONE_URL}"
+if [ "$AWS_PULL_REQUEST_ID" == "" ] || [ "$AWS_CLONE_URL" == "" ]
+then 
+    echo "error: parameter is required"
+    exit 0
+else
+    echo "AWS PULL REQUEST ID: ${AWS_PULL_REQUEST_ID}, AWS_CLONE_URL: ${AWS_CLONE_URL}"
 
-# execute node file
-node utils.js --AWS_PR_ID=$AWS_PULL_REQUEST_ID --AWS_CLONE_URL=$AWS_CLONE_URL
+    # execute node file
+    node utils.js --AWS_PR_ID=$AWS_PULL_REQUEST_ID --AWS_CLONE_URL=$AWS_CLONE_URL
+fi
